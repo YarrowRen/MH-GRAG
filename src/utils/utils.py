f50be9a1441db9f8bfb92728bb021e7d95fc3a70
YarrowRen <ywrby0214@gmail.com>
2024-11-17 13:55:30 +1300

    Remove Unnecessary Files

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -14,6 +14,16 @@ def generate_random_string(length=8):
     return ''.join(random.choice(letters) for i in range(length))
 
 def communities_to_labels(communities, num_nodes):
+    """
+    将社区分配转换为节点标签列表。
+    
+    参数:
+    communities (list of list of int): 社区列表，每个社区是节点的列表。
+    num_nodes (int): 图中节点的总数。
+    
+    返回:
+    list of int: 节点标签列表，其中每个索引对应一个节点，值为该节点所属的社区标签。
+    """
     labels = [-1] * num_nodes
     for label, community in enumerate(communities):
         for node in community:
